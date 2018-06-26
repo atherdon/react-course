c2f4d14527c198da624c613ef572ea2ddb5d4f38
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-06-26 19:38:39 +0300

    Create graphql.ml

---
+++ graphql.ml
@@ -0,0 +1,20 @@
+My Name is Arthur, And I'm a Groceristar founder.
+We're trying to build an affordable meal planning software for Foodies
+
+I build it because it's fun and can make a difference.
+
+I was working on this project 1 year alone. Then i decide to form a team of interns,
+that want to get an experience in real startup. 
+And i did it. 
+Right now we have about 15 active team members.
+
+We also working at machine learning field
+We trying to teach a model to help people pick a healthy meal
+
+I want to build a framework, that can be used in another food tech projects.
+
+Dietology, fitness, food delivery, et setera.
+
+So if a new startup will save time and money by using our code - I'll be happy.
+
+Marketing and sales is our points of weakness.
